df9ca4ec6ebd70f451051a4d71777536efbdd080
David Mikhailov <david.mikhailovv@gmail.com>
2022-02-09 15:57:49 +0400

    Create property wrappers for states

---
+++ Restart/RestartApp.swift
@@ -0,0 +1,17 @@
+//
+//  RestartApp.swift
+//  Restart
+//
+//  Created by Давид Михайлов on 04.02.2022.
+//
+
+import SwiftUI
+
+@main
+struct RestartApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
